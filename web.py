81d15af32e99539141578b2d0a54b6ded95ebdb5
ed0419 <genden80@gmail.com>
2021-09-22 01:46:57 +0800

    python logout功能

--- web.py
+++ web.py
@@ -1,9 +1,13 @@
 from datetime import timedelta
 import re, sqlite3
-from flask import Flask, request, LoginManager, render_template, redirect, session
-import json
-from threading import Thread
+from flask import Flask, request,  render_template, redirect, session
 app = Flask(__name__,static_folder='static/')
+app.config.update(
+TESTING=True,
+SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/',
+SESSION_COOKIE_NAME="WHATDOUWANT",
+#SESSION_COOKIE_DOMAIN=""
+)
 # def init_sqlite():
 #     conn = sqlite3.connect('test.db')
 #     c = conn.cursor()
@@ -17,22 +21,24 @@ app = Flask(__name__,static_folder='static/')
 #     conn.close()
 
 
-
+# 重導
 @app.route('/')
 def main():
     return redirect("login")
 
+#設置session timeout
 @app.before_request
 def make_session_permanent():
     session.permanent = True
     app.permanent_session_lifetime = timedelta(minutes=5)
 
+#登入介面 and session
 @app.route('/login',methods=["POST","GET"])
 def login():
     if 'login' not in session:
         session['login'] = 0
-    if 'stuff' not in session:
-        session['stuff'] = []
+    if 'login_uid' not in session:
+        session['login_uid'] = "OR 1=1"
     if request.method == "POST":
         uid = request.form["uid"]
         upass = request.form["upass"]
@@ -42,19 +48,33 @@ def login():
             cursor = c.execute("SELECT PASSWORD from SYS_USERS where USER='{}'".format(uid))
             result = list(cursor)[0][0]
             print(result)
-            if result == upass:
-                return render_template("admin.html",uid = uid)
+            if upass == result:
+                session['login'] = 1
+                session['login_uid'] = uid
+                return redirect("admin")
+            else:
+                return redirect("login")
         except:
             return render_template("login.html",errorMsg="錯誤的使用者帳號密碼")
     else:
         return render_template("login.html",errorMsg=" ")
 
+# 管理頁面
+@app.route('/admin')
+def admin():
+        try:
+            if session['login'] == 1:
+                return render_template("admin.html",uid=session["login_uid"])
+            else:
+                return redirect("login")
+        except:
+            return redirect("login")
+
+# 登出
+@app.route('/logout')
+def logout():
+    session['login'] = 0
+    return redirect("login")
+
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port=8080, debug=True)
-    login_manager = LoginManager(app)  
-    app.config.update(
-    TESTING=True,
-    SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/',
-    SESSION_COOKIE_NAME="WHATDOUWANT",
-    #SESSION_COOKIE_DOMAIN=""
-)
