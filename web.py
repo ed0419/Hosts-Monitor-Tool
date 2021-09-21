a5fb337067db7e30fdd838827223bb25a335db35
unknown <genden80@gmail.com>
2021-09-21 23:02:02 +0800

    create main flask backend

---
+++ web.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, render_template, redirect, url_for
+import json
+app = Flask(__name__,static_folder='static/')
+
+@app.route('/')
+def main(){
+    render_template("done_color.html")
+}
+
+@app.route("/login")
