fdc8e5d87d34f9d1eb3aed2a3eb1686336502a53
ed0419 <genden80@gmail.com>
2021-09-28 14:47:01 +0800

    完成 delete 功能 修復返回訊息顯示方式

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 import re, pymysql, json, requests
-from flask import Flask, request,  render_template, redirect, session, Response
+from flask import Flask, request,  render_template, redirect, session, url_for
 from flask_cors import CORS
 from requests.api import post
 from requests.sessions import PreparedRequest
@@ -98,9 +98,9 @@ def newhost():
                 cursor = db.cursor()
                 cursor.execute(f"INSERT INTO SYS_HOSTS (OWNEDBY, NAME, IP, PORT) VALUES ('{uid}','{hname}','{ok_hip}','{ok_hport}')")
                 db.commit()
-                return render_template("new.html",uid=uid,ErrorMsg="成功")
+                return '<script>alert("成功")</script><meta http-equiv="refresh" content="0; url=".">'
             except:
-                return render_template("new.html",ErrorMsg="Faild To Connect SQL Server")
+                return '<script>alert("Faild")</script><meta http-equiv="refresh" content="0; url=".">'
         else:
             return redirect("/admin")
         
@@ -110,41 +110,59 @@ def newhost():
         else:
             return redirect("/admin")
 
-@app.route("/delete")
+@app.route("/delete",methods=["POST","GET"])
 def delete_host():
+    Msg =""
     if 'login' not in session:
         session['login'] = 0
     if 'login_uid' not in session:
         session['login_uid'] = "X"
     try:
-        if session['login'] == 1:
-            uid=session["login_uid"]
-            html = ""
-            #SQL CONN
-            db = pymysql.connect(host="oapw.mc2021.net",user="hmt",passwd="12345678",database="hmt_data")
-            cursor = db.cursor()
-            cursor.execute(f"SELECT SERVER_ID, NAME, IP, PORT from SYS_HOSTS where OWNEDBY='{uid}'")
-            results = cursor.fetchall()
-            print(results)
-            for i in range(len(results)):
-                server_id = results[i][0]
-                name = results[i][1]
-                ip = results[i][2]
-                port = results[i][3]
-                print(server_id,name,ip,port)
-                html +=f' \
-                    <tr> \
-                        <td>{server_id}</td>\
-                        <td><a href="/server/{ip}">{ip}</a></td>\
-                        <td class="hostname">{name}</td>\
-                        <td><form action="/delete/?ip={ip}" method="POST"><a type=">確認刪除</a></td>\
-                    </tr> '
-            
-            return render_template("delete.html",uid=uid,html=html)
-        else:
-            return redirect("/admin")
+        if request.method == "GET":
+            if session['login'] == 1:
+                uid=session["login_uid"]
+                html = ""
+                #SQL CONN
+                db = pymysql.connect(host="oapw.mc2021.net",user="hmt",passwd="12345678",database="hmt_data")
+                cursor = db.cursor()
+                cursor.execute(f"SELECT SERVER_ID, NAME, IP, PORT from SYS_HOSTS where OWNEDBY='{uid}'")
+                results = cursor.fetchall()
+                print(results)
+                for i in range(len(results)):
+                    server_id = results[i][0]
+                    name = results[i][1]
+                    ip = results[i][2]
+                    port = results[i][3]
+                    print(server_id,name,ip,port)
+                    html +=f' \
+                        <tr> \
+                            <td>{server_id}</td>\
+                            <td><a href="/server/{ip}">{ip}</a></td>\
+                            <td class="hostname">{name}</td>\
+                            <td><form action="#" method="POST"><input type="hidden" name="hip" value={ip}><button type="submit" class="btn btn-sm btn-block btn-outline-primary">確認刪除</button></form></td>\
+                        </tr> '
+                
+                return render_template("delete.html",uid=uid,html=html)
+            else:
+                return redirect("/admin")
+        if request.method == "POST":
+            if session['login'] == 1:
+                print(request.path)
+                delete_ip = request.form['hip']
+                print(delete_ip)
+                #SQL CONN
+                db = pymysql.connect(host="oapw.mc2021.net",user="hmt",passwd="12345678",database="hmt_data")
+                cursor = db.cursor()
+                cursor.execute(f"DELETE FROM SYS_HOSTS WHERE IP='{delete_ip}'")
+                db.commit()
+                return '<script>alert("成功")</script><meta http-equiv="refresh" content="0; url=".">'
+            else:
+                return redirect("/admin")
     except:
-        return render_template("delete.html",uid=uid,html=html)
+        print("Error")
+        return render_template("delete.html",uid=session['login_uid'],Msg="Error")
+            
+
 
 # 管理頁面
 @app.route('/admin')
@@ -153,6 +171,8 @@ def admin():
         session['login'] = 0
     if 'login_uid' not in session:
         session['login_uid'] = "X"
+    if 'message' not in session:
+        session['message'] = ""
     try:
         if session['login'] == 1:
             uid=session["login_uid"]
